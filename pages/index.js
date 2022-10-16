1be1cb434cff7f07bdc77d61602945bbfea1059c
lucasaugsue <lucasaugsue7@gmail.com>
2022-10-16 15:46:13 -0300

    inital commit

---
+++ pages/index.js
@@ -0,0 +1,27 @@
+import Head from 'next/head';
+// import ImageBanner from '../src/components/ImageBanner';
+// import Header from '../src/components/Header';
+// import "./index.scss";
+
+export default function Home() {
+  return (
+    <div>
+      Hello world! Psiu te amo vivi!
+      {/* <Head>
+        <title>Create Next App</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head> */}
+
+      {/* <img
+        src={'/moon.jpg'}
+        style={{
+          width: '100%',
+          borderBottomLeftRadius: 350,
+          borderBottomRightRadius: 350,
+        }}
+      /> */}
+      {/* <Header/> */}
+      {/* <ImageBanner/> */}
+    </div>
+  )
+}
