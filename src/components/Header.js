f2ec072caafd826e74e41e325a1a4c4d3061681e
lucasaugsue <lucasaugsue7@gmail.com>
2022-10-25 21:09:19 -0300

    imageCasal e header transparente

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,46 +1,66 @@
-import React from 'react'
-import Link from 'next/link'
-import styles from './Header.module.css'
+import React from 'react';
+import styles from './Header.module.css';
 
-export default function Header(){
+export default function Header({transparent : transparentProps = true}){
+    const [scrollPosition, setScrollPosition] = React.useState(0);
+    const [transparent, setTransparent] = React.useState(false);
 
-    return <div className={styles.boxHeader}>
-        <div className={styles.gridContainerLogo}>
-            <div className={styles.gridItem}></div>
-            <div className={styles.gridItem}>
-                <div className={styles.logo}>L</div>
+    const handleScroll = e => {
+        setScrollPosition(document.documentElement.scrollTop)
+    }
+
+    React.useEffect(() => {
+        setTransparent(scrollPosition <= 100)
+    }, [scrollPosition])
+
+    React.useEffect(() => {
+        window.addEventListener('scroll', handleScroll)
+        return () => window.removeEventListener('scroll', handleScroll)
+    }, [])
+
+    React.useEffect(() => {
+        console.log("transparent",transparent)
+    }, [transparent])
+
+    return <div>
+        <div className={
+            transparent
+            ? styles.basicHeaderTransparent
+            : styles.basicHeader
+        }>
+            <div className={styles.gridContainerLogo}>
+                <div className={styles.gridItem}></div>
+                <div className={styles.gridItem}>
+                    <div className={styles.logo}>L</div>
+                </div>
+                <div className={styles.gridItem}>
+                    <div className={styles.logo}>&</div>
+                </div>
+                <div className={styles.gridItem}>
+                    <div className={styles.logo}>V</div>
+                </div>
+                <div className={styles.gridItem}></div>
             </div>
-            <div className={styles.gridItem}>
-                <div className={styles.logo}>&</div>
+
+            <div className={styles.gridContainerText}>
+                <div className={styles.gridItem}></div>
+                <div className={styles.gridItem}>
+                    <div className={styles.headerText}>Bem-vindos</div>
+                </div>
+                <div className={styles.gridItem}>
+                    <div className={styles.headerText}>Blog</div>
+                </div>
+                <div className={styles.gridItem}>
+                    <div className={styles.headerText}>Confirme sua presença</div>
+                </div>
+                <div className={styles.gridItem}>
+                    <div className={styles.headerText}>Entrar em contato</div>
+                </div>
+                <div className={styles.gridItem}>
+                    <div className={styles.headerText}>Livro de visitas</div>
+                </div>
+                <div className={styles.gridItem}></div>
             </div>
-            <div className={styles.gridItem}>
-                <div className={styles.logo}>V</div>
-            </div>
-            <div className={styles.gridItem}></div>
-        </div>
-        <div className={styles.gridContainerText}>
-            <div className={styles.gridItem}></div>
-            <div className={styles.gridItem}>
-                {/* <Link href="/#bem-vindos">Bem-vindos</Link> */}
-                <div className={styles.headerText}>Bem-vindos</div>
-            </div>
-            <div className={styles.gridItem}>
-                {/* <Link href="/#blog">Blog</Link> */}
-                <div className={styles.headerText}>Blog</div>
-            </div>
-            <div className={styles.gridItem}>
-                {/* <Link href="/#confirmar-presenca">Confirme sua presença</Link> */}
-                <div className={styles.headerText}>Confirme sua presença</div>
-            </div>
-            <div className={styles.gridItem}>
-                {/* <Link href="/#contato">Entrar em contato</Link> */}
-                <div className={styles.headerText}>Entrar em contato</div>
-            </div>
-            <div className={styles.gridItem}>
-                {/* <Link href="/#visitas">Livro de visitas</Link> */}
-                <div className={styles.headerText}>Livro de visitas</div>
-            </div>
-            <div className={styles.gridItem}></div>
         </div>
     </div> 
 }
--- src/components/ImagemDoCasal.js
+++ src/components/ImagemDoCasal.js
@@ -3,20 +3,14 @@ import styles from './ImagemDoCasal.module.css'
 
 
 export default function ImagemDoCasal(){
-    
+
     return <div>
-        <img className={styles.imageCasal} alt="" src="/casaleapedra.jpeg" />
+        <div className={styles.imageCasal}/>
         
         <div className={styles.gridContainer}>
             <div className={styles.gridItem}></div>
             <div style={{marginLeft: "8.75rem"}} className={styles.gridItem}>
-                <div className={styles.textoImage}>Lucas</div>
-            </div>
-            <div className={styles.gridItem}>
-                <div className={styles.textoImage}>&</div>
-            </div>
-            <div className={styles.gridItem}>
-                <div className={styles.textoImage}>Victória</div>
+                <div className={styles.textoImage}>Lucas & Victória</div>
             </div>
             <div className={styles.gridItem}></div>
         </div>
