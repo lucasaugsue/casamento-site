5d7f784fe2b96f86e61b92c4436f4b288e595403
lucasaugsue <lucasaugsue7@gmail.com>
2022-10-26 21:07:46 -0300

    simpelsmente a funcao textRotate que o pai é brabo

--- src/components/ImagemDoCasal.js
+++ src/components/ImagemDoCasal.js
@@ -3,12 +3,32 @@ import styles from './ImagemDoCasal.module.css'
 
 
 export default function ImagemDoCasal(){
+    const textRotate = () => {
+        let deg = 6;
+        let frase = "Lucas & Victória"
+
+        return (
+            <div className={styles.containerRotate}>
+                {frase.split("").map((letra, index) => {
+                    deg = 10 * (index + 1)
+
+                    return <div 
+                        style={{
+                            transform: 'rotate(' + deg + 'deg)'
+                        }}
+                    >{letra}</div>
+                })}
+            </div>
+        )
+    }
 
     return <div className={styles.container}>
         <div className={styles.image}/>
         
         <div className={styles.divText}>
-            <div className={styles.text}>Lucas & Victória</div>
+            <div className={styles.text}>
+                {textRotate()}
+            </div>
         </div>
     </div> 
 }
