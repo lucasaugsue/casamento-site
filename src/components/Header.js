ccfff80e3cb2ec25e5558dfc5c70d3f3fb32207f
lucasaugsue <lucasaugsue7@gmail.com>
2022-11-02 11:48:30 -0300

    header e font do google

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,9 +1,19 @@
+import Menu from '@mui/icons-material/Menu';
+import { Collapse, List, ListItemButton, ListItemText } from '@mui/material';
 import React from 'react';
 import styles from './Header.module.css';
 
 export default function Header({transparent : transparentProps = true}){
     const [scrollPosition, setScrollPosition] = React.useState(0);
     const [transparent, setTransparent] = React.useState(false);
+    const [open, setOpen] = React.useState(false)
+
+    const listItens = [
+        {id: 1, title: "Vamos nos casar"},
+        {id: 2, title: "Confirme sua presença"},
+        {id: 3, title: "Entrar em contato"},
+        {id: 4, title: "Livro de visitas"},
+    ]
 
     const handleScroll = e => {
         setScrollPosition(document.documentElement.scrollTop)
@@ -18,10 +28,6 @@ export default function Header({transparent : transparentProps = true}){
         return () => window.removeEventListener('scroll', handleScroll)
     }, [])
 
-    // React.useEffect(() => {
-    //     console.log("transparent",transparent)
-    // }, [transparent])
-
     return <div>
         <div className={
             transparent
@@ -29,38 +35,36 @@ export default function Header({transparent : transparentProps = true}){
             : styles.basicHeader
         }>
             <div className={styles.gridContainerLogo}>
-                <div className={styles.gridItem}></div>
-                <div className={styles.gridItem}>
+                <div className={styles.itemLogo}>
                     <div className={styles.logo}>L</div>
-                </div>
-                <div className={styles.gridItem}>
                     <div className={styles.logo}>&</div>
-                </div>
-                <div className={styles.gridItem}>
                     <div className={styles.logo}>V</div>
                 </div>
-                <div className={styles.gridItem}></div>
+                <div/>
+                <div 
+                    className={styles.itemIcon}
+                    onClick={() => setOpen(!open)} 
+                >
+                    <Menu className={styles.icon}/>
+                </div>
             </div>
-
-            <div className={styles.gridContainerText}>
-                <div className={styles.gridItem}></div>
-                <div className={styles.gridItem}>
-                    <div className={styles.headerText}>Bem-vindos</div>
-                </div>
-                <div className={styles.gridItem}>
-                    <div className={styles.headerText}>Blog</div>
-                </div>
-                <div className={styles.gridItem}>
-                    <div className={styles.headerText}>Confirme sua presença</div>
-                </div>
-                <div className={styles.gridItem}>
-                    <div className={styles.headerText}>Entrar em contato</div>
-                </div>
-                <div className={styles.gridItem}>
-                    <div className={styles.headerText}>Livro de visitas</div>
-                </div>
-                <div className={styles.gridItem}></div>
-            </div>
+            <Collapse 
+                in={open}
+                timeout="auto" 
+                unmountOnExit
+                className={styles.collapse}  
+            >
+                <List 
+                    component="div" 
+                    disablePadding
+                    className={styles.list} 
+                >
+                    {listItens.map((i) => 
+                    <ListItemButton sx={{ pl: 1 }}>
+                        <ListItemText primary={i.title} />
+                    </ListItemButton>)}
+                </List>
+            </Collapse>
         </div>
     </div> 
 }
